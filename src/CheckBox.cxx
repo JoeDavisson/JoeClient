417ed15d56cc97ae3f33cc1605871bf1d94e9d8d
JoeDavisson <joe63074@gmail.com>
2026-04-27 12:20:05 -0500

    wrong program name

--- src/CheckBox.cxx
+++ src/CheckBox.cxx
@@ -1,20 +1,20 @@
 /*
 Copyright (c) 2026 Joe Davisson.
 
-This file is part of Rendera.
+This file is part of JoeClient.
 
-Rendera is free software; you can redistribute it and/or modify
+JoeClient is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2 of the License, or
 (at your option) any later version.
 
-Rendera is distributed in the hope that it will be useful,
+JoeClient is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with Rendera; if not, write to the Free Software
+along with JoeClient; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
 */
 
